9ec92403bfac3efbf52ac5455f8c44ee2237589d
n37le <anhminh05102004@gmail.com>
2024-01-12 17:07:21 -0500

    Add component to represent list item

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const ul = document.querySelector('ul');
 const input = document.querySelector('input');
-const button = document.querySelector('button');
+const submit = document.querySelector('button');
 function clicked(event){
     const item = input.value;
     input.value = '';
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    const del = document.createElement('button');
+    li.appendChild(span);
+    li.appendChild(del);
+    del.innerText = 'Delete';
+    span.innerText = item;
+    ul.appendChild(li);
 
 }
 
-button.addEventListener('click',clicked)
+submit.addEventListener('click',clicked)
